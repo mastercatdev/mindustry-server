57427db3c326bce208111cad5ec75dab2b5a722c
mastercatdev <picat.jules@gmail.com>
2026-09-25 18:05:07 +0200

    1

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Mindustry Server Startup Script
+# Mindustry Server Startup Script for Docker
 cd /server
 
-# Run server with proper arguments
+# Set headless mode explicitly
+export HEADLESS=true
+
+# Run server with all necessary arguments
 exec java \
   -Djava.net.preferIPv4Stack=true \
   -Djava.net.preferIPv4Addresses=true \
   -Xmx2G \
   -Xms1G \
   -jar server.jar \
-  -port 6567
+  -port 6567 \
+  -mode attack \
+  -map attack
